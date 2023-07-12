18e5823340ea01a83ded4c4bd429a7f0219e3adf
Alfredo Alejandro Arguinzones <arguinzones.alejandro@gmail.com>
2023-07-12 09:19:22 -0400

    added: portfolio page

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,7 +1,88 @@
+import React from "react";
+import Projects from "./projects";
 const portfolio = () => {
   return (
     <>
-      <h1>Portfolio</h1>
+      <main>
+        <section id="portfolio">
+          <div className="aling-content-center title-page-style">
+            <div className="main-title">
+              <h1 className="title-page">
+                Experience <span className="bg-text">Portfolio</span>
+              </h1>
+            </div>
+          </div>
+        </section>
+        <section>
+          {Projects.map((project) => {
+            let imgenPosition = "";
+            let positionItems = "";
+            if (project.id % 2 === 0) {
+              imgenPosition = "";
+              positionItems = "position-right-card";
+            } else {
+              imgenPosition = "img-left-content";
+              positionItems = "position-left-card";
+              console.log("ENTRE");
+            }
+            return (
+              <div className="aling-content-center" key={project.id}>
+                <div className="card">
+                  <div
+                    className={
+                      "img-card aling-content-center " +
+                      positionItems +
+                      " " +
+                      imgenPosition
+                    }
+                  >
+                    <img
+                      src={"images/" + project.imagenName}
+                      alt=""
+                      className="img-experience"
+                    />
+                  </div>
+                  <div className={"card-name " + positionItems}>
+                    <p className="margin-b-cero text-align-right">Project</p>
+                    <h3 className="margin-t-cero">{project.projectName}</h3>
+                  </div>
+                  <div
+                    className={
+                      "description " + positionItems + " text-align-right"
+                    }
+                  >
+                    <p>{project.description}</p>
+                  </div>
+                  <div
+                    className={"skills " + positionItems + " text-align-right"}
+                  >
+                    <p>{project.skills}</p>
+                  </div>
+                  <div className={"external-link " + positionItems}>
+                    <a href={project.link} className="external-link-button">
+                      <svg
+                        stroke="currentColor"
+                        fill="none"
+                        strokeWidth="2"
+                        viewBox="0 0 24 24"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        height="1em"
+                        width="1em"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                        <polyline points="15 3 21 3 21 9"></polyline>
+                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </main>
     </>
   );
 };
---
+++ src/components/projects.jsx
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    id: 0,
+    projectName: "Digital Portfolio",
+    description:
+      "I have developed a digital portfolio, which showcases my skills, achievements, and past projects. This portfolio provides a comprehensive overview of my capabilities as a professional, and highlights my experience and expertise in the field.",
+    skills: "HTML5 CSS3 JavaScript Bootstrap Mysql PHP",
+    imagenName: "me.jpg",
+    link: "https://www.alejandroarguinzones.com",
+  },
+  {
+    id: 1,
+    projectName: "Silueta's body solutions",
+    description:
+      "This project was to create the design and development of a website from scratch. The primary goal of the website was to showcase the range of services offered by the company. it was designed to enable the company to add, edit, delete, and suspend services via a database.",
+    skills: "HTML5 CSS3 Js Bootstrap Mysql PHP Laravel",
+    imagenName: "siluetas.png",
+    link: "https://alejandroarguinzones.com/pages/info.php?id=4",
+  },
+  {
+    id: 2,
+    projectName: "Autocomplete Address Form",
+    description:
+      "The Geoapify API was used to provide address suggestions to the user as they typed, helping them complete the address more quickly and accurately. The project was successful in achieving its objective, providing a more efficient way for users to input their address information.",
+    skills: "HTML5 CSS3 JavaScript Bootstrap",
+    imagenName: "search.png",
+    link: "https://alejandroarguinzones.com/pages/info.php?id=47",
+  },
+];
+
+export default projects;
